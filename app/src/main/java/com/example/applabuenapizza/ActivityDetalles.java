e020f6ee76feeaf799a7a01a4099cf8e69c09daf
ANAIDI <58673421+ANAIDI@users.noreply.github.com>
2020-07-15 09:40:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/applabuenapizza/ActivityDetalles.java
@@ -0,0 +1,14 @@
+package com.example.applabuenapizza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityDetalles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles);
+    }
+}
